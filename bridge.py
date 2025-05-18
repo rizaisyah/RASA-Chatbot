d5a7cba2d6eb0d5010356ba091f0e3c2a663d7ee
alandhar <putrallan03@gmail.com>
2025-05-18 14:42:22 +0700

    edit bridge and retrain models

--- bridge.py
+++ bridge.py
@@ -1,24 +1,22 @@
 import os
 import requests
-from telegram.ext import Updater, MessageHandler, Filters
+from telegram.ext import ApplicationBuilder, MessageHandler, ContextTypes, filters
+from telegram import Update
 from dotenv import load_dotenv
 load_dotenv()
 
-
-TELEGRAM_TOKEN = os.getenv("TELEGRAM_TOKEN")  # Atur lewat .env atau langsung string
+TELEGRAM_TOKEN = os.getenv("TELEGRAM_TOKEN")
 RASA_URL = "http://localhost:5005/webhooks/rest/webhook"
 
-def handle_message(update, context):
+async def handle_message(update: Update, context: ContextTypes.DEFAULT_TYPE):
     message = update.message.text
     sender = update.message.chat_id
 
     res = requests.post(RASA_URL, json={"sender": str(sender), "message": message})
     for r in res.json():
-        context.bot.send_message(chat_id=sender, text=r.get("text"))
+        await context.bot.send_message(chat_id=sender, text=r.get("text"))
 
 if __name__ == "__main__":
-    updater = Updater(token=TELEGRAM_TOKEN, use_context=True)
-    dp = updater.dispatcher
-    dp.add_handler(MessageHandler(Filters.text & ~Filters.command, handle_message))
-    updater.start_polling()
-    updater.idle()
+    app = ApplicationBuilder().token(TELEGRAM_TOKEN).build()
+    app.add_handler(MessageHandler(filters.TEXT & (~filters.COMMAND), handle_message))
+    app.run_polling()
